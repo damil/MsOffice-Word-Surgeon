f8129c1a5d13c11053018d40cc08db67a20db53c
dami <laurent.dami@justice.ge.ch>
2021-01-16 22:26:33 +0100

    bump to v1.06

--- lib/MsOffice/Word/Surgeon.pm
+++ lib/MsOffice/Word/Surgeon.pm
@@ -12,7 +12,7 @@ use MsOffice::Word::Surgeon::Change;
 
 use namespace::clean -except => 'meta';
 
-our $VERSION = '1.05';
+our $VERSION = '1.06';
 
 # constant integers to specify indentation modes -- see L<XML::LibXML>
 use constant XML_NO_INDENT     => 0;
